f10c93ce655ed79a95936248070ede86254d88b5
Romeo Jr Montealegre Bardillon <107736688+rmbardillon@users.noreply.github.com>
2026-04-19 14:09:56 +0800

    update start.bat to launch backend using jar file instead of Maven

--- start.bat
+++ start.bat
@@ -1,9 +1,6 @@
 @echo off
-set "JAVA_HOME=C:\Program Files\Java\jdk-22"
-set "PATH=%JAVA_HOME%\bin;%PATH%"
-
 echo Starting Basketball League Backend...
-start "Backend" cmd /k "set "JAVA_HOME=C:\Program Files\Java\jdk-22" && set "PATH=C:\Program Files\Java\jdk-22\bin;%PATH%" && cd /d C:\Users\romsk\IdeaProjects\basketball-league && mvn spring-boot:run"
+start "Backend" cmd /k "cd /d C:\Users\romsk\IdeaProjects\basketball-league && "C:\Program Files\Java\jdk-22\bin\java" -jar target\basketball-league-0.0.1-SNAPSHOT.jar"
 
 echo Starting Basketball League Frontend...
 start "Frontend" cmd /k "cd /d C:\Users\romsk\Coding\Web Development\NextJS\basketball-league && npm run dev"
